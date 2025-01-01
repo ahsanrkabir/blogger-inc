aff7c5674c82d1f5ea8ba4fa70a0c836a9f910fe
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-02 02:53:35 +0600

    Implement Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react"
+import { MdOutlineCloseFullscreen } from "react-icons/md";
+import { TiThMenu } from "react-icons/ti";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-2 text-2xl font-bold">
+        <img src='/logo.png' alt='logo' className='w-10 h-10' />
+        <span className="text-primary-100">Blogger Inc.</span>
+      </div>
+
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <div 
+          className="cursor-pointer" 
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? (
+            <MdOutlineCloseFullscreen className="text-primary-100 size-5" /> 
+          ) : (
+            <TiThMenu className="text-primary-100 size-5" />
+          )}
+        </div>
+
+        <div className={`w-full h-[calc(100%-64px)] flex flex-col items-center justify-center absolute top-16 bg-primary-200 text-secondary-100 font-medium gap-8 text-lg transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-secondary-100 text-primary-100 font-semibold">Login</button>
+          </a>
+        </div>
+      </div>
+
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium text-primary-200">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-primary-100 text-secondary-100 font-semibold">Login</button>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#67e8f9",
+          200: "#22d3ee",
+          300: "#06b6d4",
+        },
+        secondary: {
+          100: "#16181D"
+        }
+      }
+    },
   },
   plugins: [],
 };
