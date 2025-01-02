909207db4f14da838bb782f3d2de986609097bf8
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-02 22:16:43 +0600

    Integrate Router Functionalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { MdOutlineCloseFullscreen } from "react-icons/md";
 import { TiThMenu } from "react-icons/ti";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -12,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
-      <div className="flex items-center gap-2 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-2 text-2xl font-bold">
         <Image
           src='/logo.png' 
           alt='logo'
@@ -20,7 +21,7 @@ const Navbar = () => {
           h={48}
         />
         <span className="text-primary-100">Blogger Inc.</span>
-      </div>
+      </Link>
 
       {/* MOBILE MENU */}
       <div className="md:hidden">
@@ -36,25 +37,25 @@ const Navbar = () => {
         </div>
 
         <div className={`w-full h-[calc(100%-64px)] flex flex-col items-center justify-center absolute top-16 bg-primary-200 text-secondary-100 font-medium gap-8 text-lg transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-secondary-100 text-primary-100 font-semibold">Login</button>
-          </a>
+          </Link>
         </div>
       </div>
 
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium text-primary-200">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-primary-100 text-secondary-100 font-semibold">Login</button>
-        </a>
+        </Link>
       </div>
     </div>
   )
